02bb3eb742e86a3e2c9f1b962b8ada8b79d6c7f7
Misuzu Tomari <mee.15.751@gmail.com>
2021-06-22 16:16:48 +0900

    misuzu

--- index.php
+++ index.php
@@ -12,19 +12,15 @@
 
 
 
-    <section id="lightnight">
+    <section id="light-night">
         <div class="light">
-        <img src="" alt=""><a href=""></a></img>
-        <div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-        </div>
+            <img src="https://placehold.jp/200x200.png" alt=""><a href=""></a></img>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
         </div>
 
         <div class="night">
-        <img src="" alt=""><a href=""></a></img>
-        <div>
-        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-        </div>
+            <img src="https://placehold.jp/200x200.png" alt=""><a href=""></a></img>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
         </div>
 
     </section>
