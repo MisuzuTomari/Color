d82120b708dd227b8baef15c0b545f99671856c9
Emily <emiiiiiiin7@gmail.com>
2021-06-22 16:16:28 +0900

    css

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="./assets/css/main.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/main.css">
 </head>
 <body>
 <header>
