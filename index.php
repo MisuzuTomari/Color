265f5d9415bc1507ab2ef3a7e8a37845930b995d
Misuzu Tomari <mee.15.751@gmail.com>
2021-06-22 15:18:04 +0900

    edit

--- index.php
+++ index.php
@@ -5,15 +5,58 @@
 <main>
     <section id="maincvisual">
 
-
+    </section>
     <section id="fivecolor">
 
     </section>
+
+
+
     <section id="lightnight">
+        <div class="light">
+        <img src="" alt=""><a href=""></a></img>
+        <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+        </div>
+        </div>
+
+        <div class="night">
+        <img src="" alt=""><a href=""></a></img>
+        <div>
+        <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+        </div>
+        </div>
 
     </section>
-    <section id="about">
 
+
+    <section id="about">
+    <h2>About us</h2>
+    <div class="misuzu">
+        <div class="profile-img">
+        <img src="" alt="">
+        </div>
+        <div>
+        <h3>Misuzu Tomori</h3>
+        <p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem</p>
+        </div>
+        <div>
+        <img src="" alt=""><a href=""></a></img>
+        </div>
+    </div>
+
+    <div class="emily">
+    <div class="profile-img">
+        <img src="" alt="">
+        </div>
+        <div>
+        <h3>Emily Matstumoto</h3>
+        <p>ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse,</p>
+        </div>
+        <div>
+        <img src="" alt=""><a href=""></a></img>
+        </div>
+    </div>
     </section>
 
 <?php include('subscribe.php'); ?>
