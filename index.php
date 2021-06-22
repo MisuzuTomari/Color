0fcc637916f47273bbabd6bb12c4d489ca892802
Misuzu Tomari <mee.15.751@gmail.com>
2021-06-22 17:03:28 +0900

    about, light&night 作成完了！

--- index.php
+++ index.php
@@ -28,11 +28,12 @@
 
     <section id="about">
     <h2>About us</h2>
-    <div class="misuzu">
+
+    <div class="missu">
         <div class="profile-img">
         <img src="https://placehold.jp/250x325.png" alt="">
         </div>
-        <div>
+        <div class="myself">
         <h3>Misuzu Tomori</h3>
         <p>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem</p>
         </div>
@@ -42,12 +43,12 @@
     </div>
 
     <div class="emily">
-    <div class="profile-img">
-        <img src="https://placehold.jp/250x325.png" alt="">
-        </div>
-        <div>
+        <div class="myself">
         <h3>Emily Matstumoto</h3>
         <p>ipsum, quia dolor sit, amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt, ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit, qui in ea voluptate velit esse,</p>
+        </div>
+        <div class="profile-img">
+        <img src="https://placehold.jp/250x325.png" alt="">
         </div>
         <div>
         <img src="" alt=""><a href=""></a></img>
