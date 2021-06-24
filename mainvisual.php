1d088de8be829283e8b24f2d0ebcafd52b66b268
Misuzu Tomari <mee.15.751@gmail.com>
2021-06-24 15:46:49 +0900

    kawaii

--- mainvisual.php
+++ mainvisual.php
@@ -1,18 +1,7 @@
 <main>
     <section id="maincvisual">
-        <h1>Colors</h1>
-        <p class="brown">一 Brown 一</p>
-        <p class="name">BY MISSU❤︎EMILY</p>
+            <h1>Colors</h1>
+            <p class="brown">一 Brown 一</p>
+            <p class="name">BY MISSU❤︎EMILY</p>
     </section>
-
-
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-
-    <hr size="100">
-   
-    </section>
+    <hr size="80vw">
