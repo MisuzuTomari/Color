6db63cf29bdc909d196bd987f18ea7704dab3e31
Emily <emiiiiiiin7@gmail.com>
2021-06-24 16:30:54 +0900

    no message

--- mainvisual.php
+++ mainvisual.php
@@ -15,10 +15,4 @@
 
     <hr size="100">
    
-
-
-            <h1>Colors</h1>
-            <p class="brown">一 Brown 一</p>
-            <p class="name">BY MISSU❤︎EMILY</p>
-    </section>
-    <hr size="80vw">
+ 
