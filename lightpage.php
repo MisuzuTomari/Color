9b9d134d5344412304862afb9640e49c614c73bd
Emily <emiiiiiiin7@gmail.com>
2021-06-25 15:06:29 +0900

    light color 追加

--- lightpage.php
+++ lightpage.php
@@ -2,18 +2,41 @@
 <?php include('mainvisual.php'); ?>
 
 <section>
-    <div class="box0">
-        <div class="box"></div>
-        <p>#333333</p>
+    <div class="box">
+        <div class="box0"></div>
+        <p>#f0d9b0</p>
     </div>
-    <div class="box1"></div>
-    <div class="box2"></div>
-    <div class="box3"></div>
-    <div class="box4"></div>
-    <div class="box5"></div>
-    <div class="box6"></div>
-    <div class="box7"></div>
-    <div class="box8"></div>
+
+    <div class="box">
+        <div class="box1"></div>
+        <p>#eec987</p>
+    </div> 
+
+    <div class="box">
+        <div class="box2"></div>
+        <p>#dcb375</p>
+    </div>
+    
+    <div class="box">
+        <div class="box3"></div>
+        <p>#d7ab68</p>
+    </div> 
+
+    <div class="box">
+        <div class="box4"></div>
+        <p>#cea15d</p>
+    </div>
+
+    <div class="box">
+        <div class="box5"></div>
+        <p>#cd9748</p>
+    </div>
+
+    <div class="box">
+        <div class="box6"></div>
+        <p>#cd8f34</p>
+    </div>   
+    
 </section>
 
 <?php include('subscribe.php'); ?>
