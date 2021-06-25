fc779d38df7663ffc8ed715b7aa89b683915e4ab
Emily <emiiiiiiin7@gmail.com>
2021-06-25 12:19:52 +0900

    ごめんできなかったーーー

--- lightpage.php
+++ lightpage.php
@@ -2,14 +2,18 @@
 <?php include('mainvisual.php'); ?>
 
 <section>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+    <div class="box0">
+        <div class="box"></div>
+        <p>#333333</p>
+    </div>
+    <div class="box1"></div>
+    <div class="box2"></div>
+    <div class="box3"></div>
+    <div class="box4"></div>
+    <div class="box5"></div>
+    <div class="box6"></div>
+    <div class="box7"></div>
+    <div class="box8"></div>
 </section>
 
 <?php include('subscribe.php'); ?>
